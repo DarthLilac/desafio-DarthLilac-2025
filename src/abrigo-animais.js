0922288fe60961db05e9f750df099e446a3cbc6a
Andressa Rocha <dessalr@live.com>
2025-09-08 12:26:53 -0300

    feat: add animal data and implement constructor

--- src/abrigo-animais.js
+++ src/abrigo-animais.js
@@ -1,4 +1,18 @@
+const ANIMAIS = [
+    { nome: 'Rex', especie: 'cao', brinquedos: ['RATO', 'BOLA'] },
+    { nome: 'Mimi', especie: 'gato', brinquedos: ['BOLA', 'LASER'] },
+    { nome: 'Fofo', especie: 'gato', brinquedos: ['BOLA', 'RATO', 'LASER'] },
+    { nome: 'Zero', especie: 'gato', brinquedos: ['RATO', 'BOLA'] },
+    { nome: 'Bola', especie: 'cao', brinquedos: ['CAIXA', 'NOVELO'] },
+    { nome: 'Bebe', especie: 'cao', brinquedos: ['LASER', 'RATO', 'BOLA'] },
+    { nome: 'Loco', especie: 'jabuti', brinquedos: ['SKATE', 'RATO'] },
+];
+
 class AbrigoAnimais {
+    constructor() {
+        const brinquedos = ANIMAIS.flatMap(animal => animal.brinquedos);
+        this._brinquedosValidos = new Set(brinquedos);
+    }
 
   encontraPessoas(brinquedosPessoa1, brinquedosPessoa2, ordemAnimais) {
   }

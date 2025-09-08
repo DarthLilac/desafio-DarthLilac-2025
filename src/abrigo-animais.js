86e12fba697242c02904c2fca43b296808203d2e
Andressa Rocha <dessalr@live.com>
2025-09-08 19:26:49 -0300

    refactor: separate animals constant into a new file

--- src/abrigo-animais.js
+++ src/abrigo-animais.js
@@ -1,12 +1,4 @@
-const animais = [
-    { nome: 'Rex', especie: 'cao', brinquedos: ['RATO', 'BOLA'] },
-    { nome: 'Mimi', especie: 'gato', brinquedos: ['BOLA', 'LASER'] },
-    { nome: 'Fofo', especie: 'gato', brinquedos: ['BOLA', 'RATO', 'LASER'] },
-    { nome: 'Zero', especie: 'gato', brinquedos: ['RATO', 'BOLA'] },
-    { nome: 'Bola', especie: 'cao', brinquedos: ['CAIXA', 'NOVELO'] },
-    { nome: 'Bebe', especie: 'cao', brinquedos: ['LASER', 'RATO', 'BOLA'] },
-    { nome: 'Loco', especie: 'jabuti', brinquedos: ['SKATE', 'RATO'] },
-];
+import animais from "./lista-animais.js";
 
 class AbrigoAnimais {
     constructor() {
---
+++ src/lista-animais.js
@@ -0,0 +1,11 @@
+const animais = [
+    { nome: 'Rex', especie: 'cao', brinquedos: ['RATO', 'BOLA'] },
+    { nome: 'Mimi', especie: 'gato', brinquedos: ['BOLA', 'LASER'] },
+    { nome: 'Fofo', especie: 'gato', brinquedos: ['BOLA', 'RATO', 'LASER'] },
+    { nome: 'Zero', especie: 'gato', brinquedos: ['RATO', 'BOLA'] },
+    { nome: 'Bola', especie: 'cao', brinquedos: ['CAIXA', 'NOVELO'] },
+    { nome: 'Bebe', especie: 'cao', brinquedos: ['LASER', 'RATO', 'BOLA'] },
+    { nome: 'Loco', especie: 'jabuti', brinquedos: ['SKATE', 'RATO'] },
+];
+
+export default animais;
